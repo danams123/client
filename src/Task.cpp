f7a0c238fe5a1f017946a555772cc3e5454763f8
danam <dan.am5555555@gmail.com>
2020-12-30 20:41:36 +0200

    initialize

--- src/Task.cpp
+++ src/Task.cpp
@@ -1,11 +1,10 @@
 #include "../include/Task.h"
 
-Task::Task(ConnectionHandler connectionhandler, bool shouldTerminate):connectionHandler(),shouldTerminate() {
-    connectionhandler = connectionhandler;
-    shouldTerminate = shouldTerminate;
+Task::Task(ConnectionHandler *connectionHandler, bool &shouldTerminate):connectionHandler(connectionHandler)
+,shouldTerminate(shouldTerminate) {
 }
 //TODO put in the right way so we will use the same object
-clientTask::clientTask(ConnectionHandler *connectionhandler, bool shouldTerminate): Task(connectionhandler, shouldTerminate){}
+clientTask::clientTask(ConnectionHandler *connectionHandler, bool &shouldTerminate): Task(connectionHandler, shouldTerminate){}
 
 void clientTask::run() {
     while (!shouldTerminate){
@@ -13,29 +12,30 @@ void clientTask::run() {
     //TODO do we need to add an option to double the size of the buffer if needed? if the input isn't valid throw an exception?
     char buf[bufsize];
     std::cin.getline(buf, bufsize);
-    std::string line(buf);
-    int len=line.length();
-    if (!connectionHandler.sendLine(line)) {
+    char* output = connectionHandler->decode(buf);//pointer?
+    int len = strlen(output);
+    if (!connectionHandler->sendBytes(output, len)) {
+        delete output; // is it ok?
         std::cout << "Disconnected. Exiting...\n" << std::endl;
         break;
     }
+    delete output;//is it ok?
     // connectionHandler.sendLine(line) appends '\n' to the message. Therefor we send len+1 bytes.
     std::cout << "Sent " << len+1 << " bytes to server" << std::endl;
     }
 }
 
-serverTask::serverTask(ConnectionHandler *connectionhandler, bool shouldTerminate): Task(connectionhandler, shouldTerminate){}
+serverTask::serverTask(ConnectionHandler *connectionHandler, bool &shouldTerminate): Task(connectionHandler, shouldTerminate){}
 
 void serverTask::run() {
     while(!shouldTerminate) {
         std::string answer;
         // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
         // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
-        if (!connectionHandler.getLine(answer)) {
+        if (!connectionHandler->getLine(answer)) {
             std::cout << "Disconnected. Exiting...\n" << std::endl;
             break;
         }
-
         int len = answer.length();
         // A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
         // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
